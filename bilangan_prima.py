9dcaa33ac7c1e166f411a713f54f344cb13922c8
AhmadFajhri <ahmadfajhrimuhammad@gmail.com>
2024-03-05 10:53:47 +0700

    firs commit

---
+++ bilangan_prima.py
@@ -0,0 +1,20 @@
+def is_prime(n):
+  if n <= 1:
+    return False
+  elif n <= 3:
+    return True
+  elif n % 2 == 0 or n % 3 == 0:
+    return False
+  i = 5
+  while i * i <= n:
+    if n % i == 0 or n % (i + 2) == 0:
+      return False
+    i += 6
+  return True
+
+angka = int(input("Masukkan angka: "))
+
+if is_prime(angka):
+  print(angka, "adalah bilangan prima")
+else:
+  print(angka, "bukan bilangan prima")
