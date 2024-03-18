2b7a6a757be742c7203d29920371d22ee2e0afa1
AhmadFajhri <ahmadfajhrimuhammad@gmail.com>
2024-03-18 21:48:26 +0700

    menambah hitung luas persegi panjang

--- bilangan_prima.py
+++ bilangan_prima.py
@@ -1,3 +1,4 @@
+#Rumus bilangan prima
 def is_prime(n):
   if n <= 1:
     return False
@@ -18,3 +19,19 @@ if is_prime(angka):
   print(angka, "adalah bilangan prima")
 else:
   print(angka, "bukan bilangan prima")
+
+
+# Menghitung luas persegi panjang
+def hitung_luas_persegi_panjang(panjang, lebar):
+    luas = panjang * lebar
+    return luas
+
+# Input panjang dan lebar dari pengguna
+panjang = float(input("Masukkan panjang persegi panjang: "))
+lebar = float(input("Masukkan lebar persegi panjang: "))
+
+# Memanggil fungsi untuk menghitung luas
+luas_persegi_panjang = hitung_luas_persegi_panjang(panjang, lebar)
+
+# Menampilkan hasil
+print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas_persegi_panjang}")
